090e29bbde314d24d4558f170b3f54a7ca99d516
unknown <phaser005>
2021-11-18 03:53:39 -0300

    Fusion de ramas y correccion final de errores y diseño

--- PPS-LaComanda/src/app/app.component.ts
+++ PPS-LaComanda/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+//import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+//import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { timer } from 'rxjs';
+import { SplashScreen } from '@capacitor/splash-screen';
 
 @Component({
   selector: 'app-root',
@@ -15,17 +16,18 @@ export class AppComponent {
 
   constructor(
     private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    // private splashScreen: SplashScreen,
+    // private statusBar: StatusBar
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-      timer(3000).subscribe(()=> this.splash = false);
+      // this.statusBar.styleDefault();
+      // this.splashScreen.hide();
+      SplashScreen.hide();
+      timer(300).subscribe(()=> this.splash = false);
     });
   }
 }
